6ac775328c8240ff56f50269697daf58bcf8fab5
R00tB33rMan <36140389+R00tB33rMan@users.noreply.github.com>
2024-01-22 06:01:04 +0000

    Initial Commit

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/AuthSessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/AuthSessionHandler.java
@@ -161,8 +161,10 @@ public class AuthSessionHandler implements MinecraftSessionHandler {
         }
       } else {
         if (!Objects.equals(playerKey.getSignatureHolder(), playerUniqueId)) {
+          /*
           logger.warn("UUID for Player " + player.getUsername() + " mismatches! "
               + "Chat/Commands signatures will not work correctly for this player!");
+          */
         }
       }
     }
--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
@@ -936,7 +936,10 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
     boolean isPlayerAddressLoggingEnabled = server.getConfiguration()
         .isPlayerAddressLoggingEnabled();
     String playerIp =
-        isPlayerAddressLoggingEnabled ? getRemoteAddress().toString() : "<ip address withheld>";
+        isPlayerAddressLoggingEnabled
+                ? (getRemoteAddress() != null
+                ? getRemoteAddress().toString() : "<ip address null>")
+                : "<ip address withheld>";
     return "[connected player] " + profile.getName() + " (" + playerIp + ")";
   }
 
