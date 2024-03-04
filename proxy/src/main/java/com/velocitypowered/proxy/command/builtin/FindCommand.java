5cd9d5dd12dd54b8afa9d6199882884fd0a893ed
BrianMay <brianmaymc@gmail.com>
2024-03-04 21:47:01 +0100

    Fixed message not parsing properly and removed unnecessary tab completions

--- proxy/src/main/java/com/velocitypowered/proxy/command/builtin/FindCommand.java
+++ proxy/src/main/java/com/velocitypowered/proxy/command/builtin/FindCommand.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018-2023 Velocity Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.velocitypowered.proxy.command.builtin;
 
 import com.mojang.brigadier.Command;
@@ -12,7 +29,6 @@ import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import java.util.Objects;
 import java.util.Optional;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -66,9 +82,7 @@ public class FindCommand {
   private int find(final CommandContext<CommandSource> context) {
     final String player = context.getArgument("player", String.class);
     final Optional<Player> maybePlayer = server.getPlayer(player);
-    if (maybePlayer.isEmpty()
-        && !Objects.equals(player, "all")
-        && !Objects.equals(player, "current")) {
+    if (maybePlayer.isEmpty()) {
       context.getSource().sendMessage(
           CommandMessages.PLAYER_NOT_FOUND.arguments(Component.text(player))
       );
