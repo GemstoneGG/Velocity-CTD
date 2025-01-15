c07cd1b4d8137d6b4738d844e6106428328fe53d
R00tB33rMan <rootb33rman@gmail.com>
2025-01-15 00:15:58 -0500

    Oops

--- proxy/src/main/java/com/velocitypowered/proxy/redis/RedisManagerImpl.java
+++ proxy/src/main/java/com/velocitypowered/proxy/redis/RedisManagerImpl.java
@@ -43,7 +43,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
