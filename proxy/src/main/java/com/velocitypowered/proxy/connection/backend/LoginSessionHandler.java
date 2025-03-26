7d5b9620b769017b2128bce769c18237f75b4ce9
R00tB33rMan <rootb33rman@gmail.com>
2025-03-26 01:42:37 -0400

    Minor tweak

--- proxy/src/main/java/com/velocitypowered/proxy/connection/backend/LoginSessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/backend/LoginSessionHandler.java
@@ -169,11 +169,10 @@ public class LoginSessionHandler implements MinecraftSessionHandler {
               && player.getConnection().getActiveSessionHandler() instanceof ClientPlaySessionHandler clientPlaySessionHandler) {
         smc.setAutoReading(false);
         clientPlaySessionHandler.doSwitch().thenAcceptAsync((unused) -> smc.setAutoReading(true), smc.eventLoop());
-      } else {
-        if (!(player.getConnection().getActiveSessionHandler() instanceof ClientPlaySessionHandler)) {
-          // Initial login - the player is already in configuration state.
-          server.getEventManager().fireAndForget(new PlayerEnteredConfigurationEvent(player, serverConn));
-        }
+      } else if (!server.getConfiguration().isEnableConfigurationPhase()
+              && !(player.getConnection().getActiveSessionHandler() instanceof ClientPlaySessionHandler)) {
+        // Initial login - the player is already in configuration state.
+        server.getEventManager().fireAndForget(new PlayerEnteredConfigurationEvent(player, serverConn));
       }
     }
 
