28f686f94d50035cd16170906b3b10f72e560a57
Brendan Callanan <brendan.business@outlook.com>
2025-08-25 21:59:40 +0100

    chore: shading micrometer for lettuce depend

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -148,6 +148,7 @@ dependencies {
     implementation(variantOf(libs.netty.transport.native.kqueue) { classifier("osx-aarch_64") })
 
     implementation(libs.lettuce.core)
+    implementation(libs.micrometer.commons)
     implementation(libs.jopt)
     implementation(libs.terminalconsoleappender)
     runtimeOnly(libs.jline)
