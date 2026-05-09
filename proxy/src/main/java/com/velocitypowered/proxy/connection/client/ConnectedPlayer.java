77a82f7d80e0d6c8501ea7e8748bb3051c20d47e
RootBeer <rootb33rman@gmail.com>
2026-05-09 00:16:54 -0400

    Fully connected call should remain in this order

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
@@ -314,10 +314,6 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
    * Used for cleaning up resources during a disconnection.
    */
   public void disconnected() {
-    if (!fullyConnected) {
-      return;
-    }
-
     for (VelocityBossBarImplementation bar : this.bossBars) {
       bar.viewerDisconnected(this);
     }
@@ -332,10 +328,12 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
       tryQueueOnJoinTask = null;
     }
 
-    this.server.getClusterPlayerService().onPlayerDisconnect(this);
+    if (this.fullyConnected) {
+      this.server.getClusterPlayerService().onPlayerDisconnect(this);
 
-    if (this.server.isQueueEnabled()) {
-      this.server.getQueueManager().onPlayerDisconnect(this);
+      if (this.server.isQueueEnabled()) {
+        this.server.getQueueManager().onPlayerDisconnect(this);
+      }
     }
   }
 
@@ -349,7 +347,7 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
 
   /**
    * Discards any messages still being processed by the {@link ChatQueue}, and creates a fresh state for future packets.
-   * This should be used on server switches, or whenever the client resets its own 'last seen' state.
+   * This should be used on server switches or whenever the client resets its own 'last seen' state.
    */
   public void discardChatQueue() {
     // No need for atomic swap should only be called from event loop
