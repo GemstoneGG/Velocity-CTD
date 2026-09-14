de0a2a900aba95e16af64bacbbdd8008685169fa
Aaron <bauhd@gmx.net>
2026-09-13 22:49:03 -0400

    26.3 Support

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/RespawnPacket.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/RespawnPacket.java
@@ -36,7 +36,7 @@ public class RespawnPacket implements MinecraftPacket {
 
   private short difficulty;
 
-  private short gamemode;
+  private int gamemode;
 
   private String levelType = "";
 
@@ -44,7 +44,7 @@ public class RespawnPacket implements MinecraftPacket {
 
   private DimensionInfo dimensionInfo;
 
-  private short previousGamemode;
+  private int previousGamemode;
 
   private CompoundBinaryTag currentDimensionData;
 
@@ -57,9 +57,9 @@ public class RespawnPacket implements MinecraftPacket {
   public RespawnPacket() {
   }
 
-  public RespawnPacket(int dimension, long partialHashedSeed, short difficulty, short gamemode,
+  public RespawnPacket(int dimension, long partialHashedSeed, short difficulty, int gamemode,
                        String levelType, byte dataToKeep, DimensionInfo dimensionInfo,
-                       short previousGamemode, CompoundBinaryTag currentDimensionData,
+                       int previousGamemode, CompoundBinaryTag currentDimensionData,
                        @Nullable Pair<String, Long> lastDeathPosition, int portalCooldown,
                        int seaLevel) {
     this.dimension = dimension;
@@ -116,11 +116,11 @@ public class RespawnPacket implements MinecraftPacket {
     this.difficulty = difficulty;
   }
 
-  public short getGamemode() {
+  public int getGamemode() {
     return gamemode;
   }
 
-  public void setGamemode(short gamemode) {
+  public void setGamemode(int gamemode) {
     this.gamemode = gamemode;
   }
 
@@ -140,11 +140,11 @@ public class RespawnPacket implements MinecraftPacket {
     this.dataToKeep = dataToKeep;
   }
 
-  public short getPreviousGamemode() {
+  public int getPreviousGamemode() {
     return previousGamemode;
   }
 
-  public void setPreviousGamemode(short previousGamemode) {
+  public void setPreviousGamemode(int previousGamemode) {
     this.previousGamemode = previousGamemode;
   }
 
@@ -220,9 +220,17 @@ public class RespawnPacket implements MinecraftPacket {
       this.partialHashedSeed = buf.readLong();
     }
 
-    this.gamemode = buf.readByte();
+    if (version.noLessThan(ProtocolVersion.MINECRAFT_26_3)) {
+      this.gamemode = ProtocolUtils.readVarInt(buf);
+    } else {
+      this.gamemode = buf.readByte();
+    }
     if (version.noLessThan(ProtocolVersion.MINECRAFT_1_16)) {
-      this.previousGamemode = buf.readByte();
+      if (version.noLessThan(ProtocolVersion.MINECRAFT_26_3)) {
+        this.previousGamemode = ProtocolUtils.readVarInt(buf);
+      } else {
+        this.previousGamemode = buf.readByte();
+      }
       boolean isDebug = buf.readBoolean();
       boolean isFlat = buf.readBoolean();
       this.dimensionInfo = new DimensionInfo(dimensionKey, levelName, isFlat, isDebug, version);
@@ -279,9 +287,17 @@ public class RespawnPacket implements MinecraftPacket {
       buf.writeLong(partialHashedSeed);
     }
 
-    buf.writeByte(gamemode);
+    if (version.noLessThan(ProtocolVersion.MINECRAFT_26_3)) {
+      ProtocolUtils.writeVarInt(buf, this.gamemode);
+    } else {
+      buf.writeByte(this.gamemode);
+    }
     if (version.noLessThan(ProtocolVersion.MINECRAFT_1_16)) {
-      buf.writeByte(previousGamemode);
+      if (version.noLessThan(ProtocolVersion.MINECRAFT_26_3)) {
+        ProtocolUtils.writeVarInt(buf, this.previousGamemode);
+      } else {
+        buf.writeByte(this.previousGamemode);
+      }
       buf.writeBoolean(dimensionInfo.isDebugType());
       buf.writeBoolean(dimensionInfo.isFlat());
       if (version.lessThan(ProtocolVersion.MINECRAFT_1_19_3)) {
